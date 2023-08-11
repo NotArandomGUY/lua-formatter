04a89884a9dbe2e59976f84196929bb5dfd60e95
NotArandomGUY <hhin0001@gmail.com>
2023-08-12 05:10:19 +0800

    feat: Add decompiler support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { parse } from 'luaparse'
-import { basename } from 'path'
+import { Parser, parse } from 'luaparse'
+import { basename, dirname, join } from 'path'
 import { exit } from 'process'
 import LuaBase from './ast/LuaBase'
 import LuaChunk from './ast/Node/LuaChunk'
-import { FileReader, writeFile } from './lib/fileSystem'
+import { execCommand } from './lib/childProcess'
+import { FileReader, fileExists, writeFile } from './lib/fileSystem'
 import Step from './step/Step'
 import FixupFunctionNameStep from './step/deobfuscate/FixupFunctionNameStep'
 import InlineStep from './step/deobfuscate/InlineStep'
@@ -27,6 +28,49 @@ async function processChunk(mode: string, chunk: LuaChunk, maxIteration: number)
   }
 }
 
+async function readLuacFile(dir: string, reader: FileReader, parser: Parser): Promise<number> {
+  const buf = reader.readBytes(0, reader.getSize())
+
+  if (buf == null) return -1
+
+  const decompilerPath = join(dir, 'decompiler.exe')
+
+  if (!await fileExists(decompilerPath)) {
+    console.log(`Failed to find decompiler at: ${decompilerPath}`)
+    return -1
+  }
+
+  const lines = (await execCommand(`"${decompilerPath.replace(/"/g, '\\"')}" "${reader.getPath().replace(/"/g, '\\"')}"`)).split('\n')
+  let lineCount = 0
+
+  for (const line of lines) {
+    parser.write(line + '\n')
+    lineCount++
+  }
+
+  return Math.ceil(lineCount / 10)
+}
+
+async function readLuaFile(reader: FileReader, parser: Parser): Promise<number> {
+  let line = reader.readLine()
+  let lineCount = 0
+
+  while (line != null) {
+    parser.write(line + '\n')
+    line = reader.readLine()
+    lineCount++
+  }
+
+  return Math.ceil(lineCount / 10)
+}
+
+async function readFile(dir: string, reader: FileReader, parser: Parser): Promise<number> {
+  const magic = reader.readBytes(0, 4)
+  const isLuac = magic != null && Buffer.compare(magic, Buffer.from([0x1B, 0x4C, 0x75, 0x61])) === 0
+
+  return isLuac ? readLuacFile(dir, reader, parser) : readLuaFile(reader, parser)
+}
+
 async function main(argv: string[]): Promise<number> {
   const argc = argv.length
 
@@ -37,7 +81,7 @@ async function main(argv: string[]): Promise<number> {
 
   const mode = argv[2]
   const srcPath = argv[3]
-  const dstPath = argv[4] ?? (srcPath.endsWith('.lua') ? srcPath.slice(0, -4) + '.ast.lua' : srcPath)
+  const dstPath = argv[4] ?? ((srcPath.endsWith('.lua') ? srcPath.slice(0, -4) : srcPath) + '.ast.lua')
 
   const reader = new FileReader(srcPath)
 
@@ -53,21 +97,19 @@ async function main(argv: string[]): Promise<number> {
     encodingMode: 'x-user-defined'
   })
 
-  let line = reader.readLine()
-  let lineCount = 0
-
-  while (line != null) {
-    parser.write(line + '\n')
-    line = reader.readLine()
-    lineCount++
-  }
+  const result = await readFile(dirname(argv[0]), reader, parser)
 
   reader.close()
+
+  if (result < 0) {
+    console.log(`Failed to read file: ${reader.getPath()}`)
+    return 1
+  }
 
   await LuaBase.init()
   const ast = LuaBase.createFromJson(parser.end(''))
 
-  if (await processChunk(mode, ast, Math.ceil(lineCount / 10)) !== 0) return 1
+  if (await processChunk(mode, ast, result) !== 0) return 1
 
   await writeFile(dstPath, ast.toString())
 
